cb5aad1c1ff6e30b85a1deab088fcd5706d803bc
Tparfaite <parfaitetwagira@gmail.com>
2022-08-18 20:25:45 +0200

    Feat:changing file -Add all [Finishes#]

---
+++ index.js
@@ -0,0 +1,32 @@
+const body=document.querySelector(".body");
+const container=document.getElementById("container");
+const todate=document.getElementById("todate");
+
+const years=new Date();
+// console.log(years.toUTCString());
+
+const months=["January","February","March","April","May","June","July","August","September","October","November","December"];
+const month=months[years.getMonth()];
+
+const days=["Sunday","Monday","Tuesday","Wensday","Thursday","Friday","Saturday"];
+const day=days[years.getDay()];
+
+const date=years.getDate();
+const year=years.getFullYear();
+
+const hour=years.getHours();
+const minute=years.getMinutes();
+const second=years.getSeconds();
+
+todate.textContent=`${day},${month} ${date},${year}`;
+
+
+const today=()=>{
+setTimeout(()=>{
+    today()
+},1000);
+container.textContent=`${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`;
+
+
+}
+today();
